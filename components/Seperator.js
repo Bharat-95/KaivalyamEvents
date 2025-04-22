2d8cf3fec800b5404b95eb0af2223fef9d542332
G7cars <g7selfdrivecars@gmail.com>
2025-04-22 11:25:34 +0530

    a

--- components/Seperator.js
+++ components/Seperator.js
@@ -2,18 +2,19 @@
 
 import React from "react";
 
-const SectionSeparator = () => {
-  return (
-    <div
-      className="w-full h-[140px]"
-      style={{
-        backgroundImage: "url('/Seperator.png')",
-        backgroundRepeat: "repeat-x",
-        backgroundSize: "auto 100%",
-        backgroundPosition: "top center",
-      }}
-    />
+const SectionSeparator = () => (
+    <div className="w-full overflow-hidden">
+      <svg
+        width="100%"
+        height="100"
+        viewBox="0 0 1200 100"
+        preserveAspectRatio="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0,30 C300,120 900,-20 1200,60 L1200,100 L0,100 Z" fill="#c79f3e" />
+      </svg>
+    </div>
   );
-};
+  
 
 export default SectionSeparator;
